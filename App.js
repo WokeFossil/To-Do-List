c7128ee3a55be05ee0f88d9877beb0eec24ab220
WokeFossil <117059223+WokeFossil@users.noreply.github.com>
2025-10-06 02:40:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import { FlatList, Modal, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { View, Text, StyleSheet } from 'react-native';
